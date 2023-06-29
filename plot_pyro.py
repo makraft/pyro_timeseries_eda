b3207306398fb909c6a87bfe574d444cd4078f43
Kraft  Marcel <mkraft@ethz.ch>
2023-06-29 10:25:47 +0200

    Improve scatter plots

--- plot_pyro.py
+++ plot_pyro.py
@@ -1,4 +1,4 @@
-# %%
+# %%    Find data directory
 # define directory and test if path works
 import os
 
@@ -14,7 +14,7 @@ else:
 
 
 
-# %%
+# %%    Find files
 # Create set of paths to access individual files
 import pandas as pd
 
@@ -43,13 +43,13 @@ df = pd.DataFrame(file_attributes, columns=["file path", "part", "iteration", "l
 
 
 
-# %%
+# %%    Select single file
 # Find the path to a selected file
 import matplotlib.pyplot as plt
 
 # Define the criteria for selecting the file
-part = "1"  # Specify the desired part number
-iteration = "1"  # Specify the desired iteration of the layer experiment
+part = "7"  # Specify the desired part number
+iteration = "2"  # Specify the desired iteration of the layer experiment
 layer_thickness = "0" # Specify the desired layer thickness
 
 # Filter the DataFrame based on the criteria
@@ -65,9 +65,8 @@ file_path = found_files[0]['file path']
 
 
 
-# %%
-# open file as dataframe. This method is a lot faster (about 5x) than using 
-# readline() to create lists and zip those into a dataframe.
+# %%    Dataframe from single file
+# open file as dataframe
 import time
 import numpy as np
 tstart = time.time()
@@ -77,16 +76,30 @@ print("Time for importing data with pd.read_csv: {0}".format((str(time.time()-ts
 
 
 
-# %%
+# %%    Scatterplots for individual time series
 
+# Generate single part scatter plot
 # Filter the DataFrame based on the "status" column
 filtered_df = df_data[df_data['status'] == 1]
+# If desired, set limits to the colorbar. Important for comparing plots.
+set_uniform_limits = True
+max_intensity = df_data['intensity'].max()
+if set_uniform_limits:
+    colorbar_min = 0    # custom lower limit
+    colorbar_max = 155  # custom upper limit
+else:
+    # The colorbar is adjusted automatically for each plot
+    colorbar_min = df_data['intensity'].min()
+    colorbar_max = max_intensity
+if max_intensity > colorbar_max:
+    print("Warning, some large intensity values are not plotted correctly!")
+    # Adjust colorbar_max if this occurs
 # Create the scatterplot
-plt.scatter(filtered_df["x"],filtered_df["y"], c=filtered_df["intensity"], cmap="hot",s=4)
+plt.scatter(filtered_df["x"],filtered_df["y"], c=filtered_df["intensity"], cmap="hot",s=1, vmin=colorbar_min, vmax=colorbar_max)
 plt.xlabel('X Coordinate')
 plt.ylabel('Y Coordinate')
-plt.title('Scatterplot')
-plt.colorbar(label='Intensity')
+plt.title('Layer thickness {0} [micro_m], Part {1}, Iteration {2}'.format(layer_thickness, part, iteration))
+plt.colorbar(label='Intensity, Maximum = {0}'.format(max_intensity))
 plt.show()
 
 # %%
